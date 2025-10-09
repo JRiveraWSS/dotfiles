afe8a94a0705b496cd4e0f3a522cee1ab100b41a
JRiveraWSS <jhonatan@worksimpli.io>
2025-10-09 16:20:31 -0400

    feat: add lualine statusline plugin and update configs
    - Add lualine.nvim configuration for enhanced statusline
    - Add extensions.txt for GNOME Shell extension settings
    - Update ghostty config: enable background blur
    - Update kanata.kbd: fix Tarmak3 layout duplication
    - Update lazy-lock.json: plugin updates and add nvim-web-devicons
    - Update lazyvim.json: add yaml and dot language extras
    - Update options.lua: disable snacks.nvim animations

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
+
 vim.opt.wrap = true
 
 -- Use system clipboard
@@ -10,3 +10,6 @@ vim.opt.clipboard = "unnamedplus"
 -- Background and colors
 vim.opt.background = "dark"
 vim.opt.termguicolors = true
+
+-- Disable animations
+vim.g.snacks_animate = false
